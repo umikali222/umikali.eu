c64b5ac5a91f157b15d3ba5f0945539a2e9748f2
Michał Wójtowicz <80032496+umikali222@users.noreply.github.com>
2023-04-21 14:54:17 +0200

    added wine setup script for ubuntu

---
+++ uwine.sh
@@ -0,0 +1,9 @@
+sudo dpkg --add-architecture i386 
+
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+
+sudo apt install --install-recommends winehq-stable
