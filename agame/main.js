3239dab640f4839e8a4de22cf62f8a4e33968d1c
umikali222 <zelatyna123@gmail.com>
2023-07-01 07:31:11 +0200

    added a gambling engine

--- agame/main.js
+++ agame/main.js
@@ -10,12 +10,19 @@ var vendingMachineProfitPerSec = 1
 
 var marketing2upgrade = true
 var marketing3upgrade = false
+var gamblingUpgrade = false
+
+var gamblingEngine = false
+
+var updatesBeforeGambleMesssageReset = 0
 
 function refreshGame(){
     document.getElementById('money').textContent = money // update money
     document.getElementById('energy').textContent = energy // update energy
     document.getElementById('eatCost').textContent = eatCost // update eating cost
     document.getElementById('vendingMachineCost').textContent = vendingMachineCost // update vending machine cost
+    if (updatesBeforeGambleMesssageReset == 0){document.getElementById('gambleMessage').textContent = ''}
+    else{updatesBeforeGambleMesssageReset--}
     if (vendingMachines != 0) {document.getElementById('vendingMachines').textContent = vendingMachines} // update amount of vending machines
     else {document.getElementById('vendingMachines').textContent = vendingMachines}// or set it to nothing
     if (energy < 1){
@@ -44,6 +51,21 @@ function refreshGame(){
     if (money >= 100){
         document.getElementById('upgrades').style.display = 'block'
     }
+    if (document.getElementById('betAmount').value <= money){
+        document.getElementById('gambleButton').removeAttribute('disabled')
+    }else{
+        document.getElementById('gambleButton').setAttribute('disabled', 'disabled')
+    }
+    var x = document.getElementById('betAmount').value
+    if (Math.abs(x) != x){
+        document.getElementById('gambleButton').setAttribute('disabled', 'disabled')
+    }
+    if (gamblingEngine){
+        document.getElementById('gamblingDiv').style.display = 'block'
+    }else{
+        document.getElementById('gamblingDiv').style.display = 'none'
+    }
+
 
     // UPGRADES
 
@@ -61,11 +83,21 @@ function refreshGame(){
     if (marketing3upgrade){
         document.getElementById('marketing3').style.display = 'block'
         document.getElementById('marketing3').setAttribute('disabled', 'disabled') // disable marketing 3 upgrade
-        if (money >= 100){
+        if (money >= 500){
             document.getElementById('marketing3').removeAttribute('disabled') // enable it
         } 
     }else{
         document.getElementById('marketing3').style.display = 'none'
+    }
+
+    if (gamblingUpgrade){
+        document.getElementById('gambling').style.display = 'block'
+        document.getElementById('gambling').setAttribute('disabled', 'disabled') // disable marketing 3 upgrade
+        if (money >= 2500){
+            document.getElementById('gambling').removeAttribute('disabled') // enable it
+        } 
+    }else{
+        document.getElementById('gambling').style.display = 'none'
     }
 }
 
@@ -76,12 +108,28 @@ function upgrade(identifier){
             vendingMachineProfitPerSec *= 5
             marketing2upgrade = false
             marketing3upgrade = true
+            gamblingUpgrade = true
             break
         case 2:
-            money -= 100
+            money -= 500
             vendingMachineProfitPerSec *= 5
             marketing3upgrade = false
             break
+        case 3:
+            money -= 2500
+            gamblingUpgrade = false
+            gamblingEngine = true
+            break
+    }
+    refreshGame()
+}
+
+function gamble(){
+    money -= Math.abs(document.getElementById('betAmount').value)
+    if (Math.floor(Math.random() * 24) + 1 == document.getElementById('bet').value){
+        money += document.getElementById('betAmount').value * 24
+        updatesBeforeGambleMesssageReset = 10
+        document.getElementById('gambleMessage').textContent = 'Your Bet Was Right!!! you made ' + (document.getElementById('betAmount').value) * 23 + '$'
     }
     refreshGame()
 }
